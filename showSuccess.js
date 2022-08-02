316531a1f54600b84abe346c84cda7da9f05054d
PalczewskiCezary <89982725+PalczewskiCezary@users.noreply.github.com>
2022-08-02 10:57:52 +0200

    Delete showSuccess.js

--- showSuccess.js
+++
@@ -1,9 +0,0 @@
-export const showSuccess = (input) => {
-    const formitem = input.parentElement;
-    formitem.classList.remove('error');
-    formitem.classList.add('success');
-    error.textContent = '';
-    return {
-        error: error
-    }
-}
