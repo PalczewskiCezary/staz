3ffefbcd45d3eb5e4557e577be3988d30ebefb20
PalczewskiCezary <89982725+PalczewskiCezary@users.noreply.github.com>
2022-08-02 10:57:13 +0200

    Delete showError.js

--- showError.js
+++
@@ -1,9 +0,0 @@
-export const showError = (input, message) => {
-    const formitem = input.parentElement;
-    formitem.classList.remove('success');
-    formitem.classList.add('error');
-    error.textContent = message;
-    return {
-        error: error
-    }
-};
